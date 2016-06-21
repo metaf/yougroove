7fcfa1789ac88351e9e8225b1e64326a7995d1a4
metaf <taf34@pitt.edu>
2016-06-21 14:16:41 -0400

    start by following official flask tutorial

---
+++ blog.py
@@ -0,0 +1,33 @@
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'blog.db'),
+    SECRET_KEY='development_key',
+    USERNAME='admin',
+    PASSWORD='default'))
+
+app.config.from_envvar('BLOG_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the db"""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """grab existing or create new db connect"""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db=connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """closes db at end of request"""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
